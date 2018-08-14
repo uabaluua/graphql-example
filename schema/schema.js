e4cabd1074a59cb56fa1a1c10562cc65d86707bd
Dmytro Yehorov <d.yehorov@svitla.com>
2018-08-14 10:32:49 +0300

    init

---
+++ schema/schema.js
@@ -0,0 +1,35 @@
+const graphql = require('graphql')
+const axios = require('axios')
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLSchema,
+} = graphql
+
+const UserType = new GraphQLObjectType({
+  name: 'User',
+  fields: {
+    id: { type: GraphQLString },
+    firstName: { type: GraphQLString },
+    age: { type: GraphQLInt },
+  },
+})
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    user: {
+      type: UserType,
+      args: {id: {type: GraphQLString}},
+      resolve: (parentValue, args) => {
+        return axios.get(`http://localhost:3030/users/${args.id}`)
+          .then(response => response.data)
+      },
+    }
+  }
+})
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+})
